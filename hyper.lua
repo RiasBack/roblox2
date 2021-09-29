b68c335f327c9c90352a49ddb3962b959e75fff5
sdgsdgsdgewgdwegdwg <88737709+RiasBack@users.noreply.github.com>
2021-09-29 14:24:33 +0700

    Add files via upload

---
+++ hyper.lua
@@ -0,0 +1,3 @@
+local discordinv = "https://discord.gg/8FkZNVPCEs"
+game.Players.LocalPlayer:Kick("Join Discord Server for get new script! in#script-here Invite link is copied. "..discordinv)
+setclipboard(discordinv)
